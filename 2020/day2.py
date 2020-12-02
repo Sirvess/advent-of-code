9d916f37834f4a3f2ee3e275b18c9fb585e876ab
Sirvess <joe.slask@gmail.com>
2020-12-02 12:03:58 +0100

    Formatting

--- 2020/day2.py
+++ 2020/day2.py
@@ -1,22 +1,51 @@
 import re
 
-def countValidPasswords(data,rule):
+
+def countValidPasswords(data, rule):
     if(rule == "count"):
-        return len(list(filter(lambda node: len(re.findall(node["char"],node["pw"])) in range(node["a"],node["b"]+1) ,data)))
+        return len(
+            list(
+                filter(
+                    lambda node: len(
+                        re.findall(
+                            node["char"],
+                            node["pw"])) in range(
+                        node["a"],
+                        node["b"] +
+                        1),
+                    data)))
     elif(rule == "pos"):
-        return len(list(filter(lambda node: [node["pw"][node["a"]-1] == node["char"],node["pw"][node["b"]-1] == node["char"]].count(True) == 1,filter(lambda node: len(node["pw"]) >= node["b"]-1 ,data))))
+        return len(list(filter(lambda node: [node["pw"][node["a"] - 1] == node["char"],
+                                             node["pw"][node["b"] - 1] == node["char"]].count(True) == 1,
+                               filter(lambda node: len(node["pw"]) >= node["b"] - 1,
+                                      data))))
     return "Invalid rule."
 
+
 def formatInput(data):
-    return map(lambda x: {"a": int(x[0]),"b": int(x[1]),"char": x[2][:-1],"pw": x[3]},
-                        map(lambda node: re.split("[\n -]",node,maxsplit=4),
-                            inData))
+    return map(lambda x: {"a": int(x[0]),
+                          "b": int(x[1]),
+                          "char": x[2][:-1],
+                          "pw": x[3]},
+               map(lambda node: re.split("[\n -]",
+                                         node,
+                                         maxsplit=4),
+                   inData))
+
 
 if __name__ == "__main__":
-    f = open("day2Input.txt","r") 
-    inData = f.readlines() 
+    f = open("day2Input.txt", "r")
+    inData = f.readlines()
     f.close()
 
     # Results
-    print("Valid according to count: ", countValidPasswords(formatInput(inData),"count"))
-    print("Valid according to position: ", countValidPasswords(formatInput(inData),"pos"))
+    print(
+        "Valid according to count: ",
+        countValidPasswords(
+            formatInput(inData),
+            "count"))
+    print(
+        "Valid according to position: ",
+        countValidPasswords(
+            formatInput(inData),
+            "pos"))
