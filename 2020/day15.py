7211d1dc2e4f8b7d2156e7f58bfb0e62f6f3b745
Sirvess <joe.slask@gmail.com>
2020-12-15 07:09:01 +0100

    Day15 cleanup

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,7 +1,7 @@
 def getNo(iterations, data):
     cache = {}
     for i in range(0, len(data)):
-        cache[data[i]] = {"c": 1, "l": i + 1, "r": i + 1}
+        cache[data[i]] = {"l": i + 1, "r": i + 1}
 
     prev = data[-1]
     i = len(data) + 1
@@ -9,9 +9,8 @@ def getNo(iterations, data):
         nxt = cache[prev]["r"] - cache[prev]["l"]
 
         if nxt not in cache:
-            cache[nxt] = {"c": 1, "l": i, "r": i}
+            cache[nxt] = {"l": i, "r": i}
         else:
-            cache[nxt]["c"] += 1
             cache[nxt]["l"] = cache[nxt]["r"]
             cache[nxt]["r"] = i
 
