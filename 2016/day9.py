653b876ec83d08520ebe791aa390c245194cc443
Sirvess <joe.slask@gmail.com>
2021-01-13 18:05:08 +0100

    Day 9 Part B

--- 2016/day9.py
+++ 2016/day9.py
@@ -3,18 +3,29 @@ if __name__ == "__main__":
     data = f.read().splitlines()[0]
     f.close()
 
-    outstr = ""
-    i = 0
-    while i < len(data):
-        if data[i] == "(":
-            stop = i + data[i:].index(")")
-            nums = data[i + 1 : stop].split("x")
-            numchars = int(nums[0])
-            repeater = int(nums[1])
-            for j in range(repeater):
-                outstr += data[stop + 1 : stop + 1 + numchars]
-            i = stop + numchars + 1
-        else:
-            outstr += data[i]
-            i += 1
-    print("Part A:", len(outstr))
+    def decompressStr(data, mode="A"):
+
+        outstr = ""
+        i = 0
+        while i < len(data):
+            if data[i] == "(":
+                stop = i + data[i:].index(")")
+                nums = data[i + 1 : stop].split("x")
+                numchars = int(nums[0])
+                repeater = int(nums[1])
+
+                if mode == "B":
+                    substr = decompressStr(data[stop + 1 : stop + numchars + 1], "B")
+                else:
+                    substr = data[stop + 1 : stop + 1 + numchars]
+
+                for j in range(repeater):
+                    outstr += substr
+                i = stop + numchars + 1
+            else:
+                outstr += data[i]
+                i += 1
+        return outstr
+
+    print("Part A:", len(decompressStr(data)))
+    print("Part B:", len(decompressStr(data, mode="B")))
