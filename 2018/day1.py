c09881fcfed084afc73f545f5bdf553f4ce6b4ce
Sirvess <joe.slask@gmail.com>
2021-02-09 22:40:27 +0100

    2018 Day 1

---
+++ 2018/day1.py
@@ -0,0 +1,19 @@
+if __name__ == "__main__":
+    f = open("day1.in", "r")
+    data = [int(x) for x in f.read().splitlines()]
+    f.close()
+
+    print("Part a", sum(data))
+
+    frequencies = set()
+    curr, found = 0, False
+    while True:
+        for x in data:
+            curr += x
+            if curr in frequencies:
+                print("Part B", curr)
+                found = True
+                break
+            frequencies.add(curr)
+        if found:
+            break
